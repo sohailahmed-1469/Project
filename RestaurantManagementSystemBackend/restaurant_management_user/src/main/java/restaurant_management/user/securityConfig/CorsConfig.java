f636caf8d15a6d5704c12018e9147d466b1398d8
sohailahmed-1469 <suhailahmed.mohammad@gmail.com>
2026-05-14 02:14:55 +0800

    Update CorsConfig.java

--- RestaurantManagementSystemBackend/restaurant_management_user/src/main/java/restaurant_management/user/securityConfig/CorsConfig.java
+++ RestaurantManagementSystemBackend/restaurant_management_user/src/main/java/restaurant_management/user/securityConfig/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins(
+                       .allowedOriginPatterns(      
                                 "http://localhost:5173",
                                 "https://*.vercel.app"
                         )
