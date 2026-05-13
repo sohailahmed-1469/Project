d019b8a2fe87054c9787e309685a26b7f0ed4de3
Sohail Ahmed Mohammad <sohailahmed.mohammad@cjsstech.com>
2026-05-14 01:21:24 +0800

    updated cors config

--- RestaurantManagementSystemBackend/restaurant_management_user/src/main/java/restaurant_management/user/securityConfig/CorsConfig.java
+++ RestaurantManagementSystemBackend/restaurant_management_user/src/main/java/restaurant_management/user/securityConfig/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://project-snowy-eight-33.vercel.app"
+                                "https://*.vercel.app"
                         )
                         .allowedMethods(
                                 "GET",
