9d8ea99a7754ccf35acfa328f9cc43d1990a3194
sohailahmed-1469 <suhailahmed.mohammad@gmail.com>
2026-05-13 17:22:52 +0800

    Update CorsConfig.java

--- RestaurantManagementSystemBackend/restaurant_management_user/src/main/java/restaurant_management/user/securityConfig/CorsConfig.java
+++ RestaurantManagementSystemBackend/restaurant_management_user/src/main/java/restaurant_management/user/securityConfig/CorsConfig.java
@@ -10,16 +10,27 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://project-snowy-eight-33.vercel.app"
+                        )
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
 }
-
